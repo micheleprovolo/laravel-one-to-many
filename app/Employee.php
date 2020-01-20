7bf3cfe7cbaa18274ec141f1178416a0df91e34c
michele <michele.provolo@gmail.com>
2020-01-20 16:33:02 +0100

    create le migration e i model

---
+++ app/Employee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'name',
+        'lastname',
+        'birth_year'
+    ];
+
+    public function tasks() {
+        
+        return $this -> hasMany(Task::class);
+    }
+
+}
